5988d3390b642d7594a6633ea6d68d514e685e62
EduardoFORodrigues <dudacriativo@hotmail.com>
2025-08-13 11:46:33 -0300

    first commit

---
+++ REadme.md
@@ -0,0 +1,6 @@
+# consulta de link https://www.submarinecablemap.com/
+# cobertura de iternet https://www.submarinecablemap.com/
+# https://www.npmjs.com/package/lodash
+# lodast pacote que simplifica código
+# nodemon ele atualiza automaticamente
+# Em casa  npx nodemon servidor.js
---
+++ servidor.js
@@ -0,0 +1,70 @@
+const http = require("http");
+const fs = require("fs");
+const _ = require("lodash");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+
+  //definindo o tipo deconteúdo do cabeçalho
+  res.setHeader("Tipo-Conteudo", "texto/html");
+
+  //caminho dos arquivos
+
+  let caminho = "./views/";
+
+  switch (req.url) {
+    case "/":
+      caminho += "index.html";
+      res.statusCode = 200;
+      break;
+
+    case "/servico":
+      caminho += "servico.html";
+      res.statusCode = 200;
+      break;
+
+    case "/tecnologias":
+      caminho += "tecnologias.html";
+      res.statusCode = 200;
+      break;
+
+    case "/contato":
+      caminho += "contato.html";
+      res.statusCode = 200;
+      break;
+
+    //redirecionamento
+    case "/contato":
+      res.statusCode = 301;
+      res.setHeader("Location", "/contato");
+      break;
+
+    default:
+      caminho += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  //enviando um arquivo html
+  fs.readFile(caminho, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      //res.write(data);
+      // res.end();
+      res.end(data); //ou forma de encurtar código
+    }
+  });
+});
+
+const numero = _.random(0, 50);
+const saudacao = _.once(() => {
+  console.log("Boas Vindas");
+});
+saudacao();
+console.log(numero);
+
+server.listen(3009, "localhost", () => {
+  console.log("Ovindo o servidor na porta 3009");
+});
